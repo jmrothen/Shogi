fb1a913c56acd20d58b628d309c1727c6c310541
John Rothen <rackjothen@gmail.com>
2024-08-10 17:47:51 -0400

    added player turn indicator, improved pockets

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from pygame.locals import *
 # Initialize the game
 pygame.init()
 
-# Set up the screen
+#####################
+# Set up the screen #
+#####################
+
 # screen parameters
 screen_height = 1000
 screen_width = 1300
@@ -17,15 +20,6 @@ board_width = board_height
 
 # calculate the cell size
 cell_size = board_width // 9
-
-
-# create a function break a gap into two equal-ish parts
-def adjusted_half_gap(dimension_one, dimension_two):
-    if dimension_one - dimension_two % 2 == 0:
-        return (dimension_one - dimension_two) / 2, (dimension_one - dimension_two) / 2
-    else:
-        return math.ceil((dimension_one - dimension_two) / 2), math.floor((dimension_one - dimension_two) / 2)
-
 
 # calculate the gaps at top and bottom of the screen
 height_gap_top, height_gap_bot = adjusted_half_gap(screen_height, board_height)
@@ -52,11 +46,17 @@ IVORY = (255, 255, 240)
 GRAY = (128, 128, 128)
 
 
+##########################
+# Function to draw board #
+##########################
+
+
 # Function to draw the Shogi board
 def draw_shogi_board_pygame(pa=None, save_path=None):
+    # reference global variables
     global selected_piece
     global se_pos
-
+    global error_text
     if not pa:
         global piece_array
     else:
@@ -65,7 +65,7 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
     # Fill background
     screen.fill(BEIGE)
 
-    # Draw the board
+    # Draw the game board
     pygame.draw.rect(screen, BLACK, (width_gap, height_gap_top, board_height, board_width), 3)
     for i in range(1, 9):
         pygame.draw.line(screen, BLACK, (width_gap, height_gap_top + i * cell_size),
@@ -73,7 +73,7 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
         pygame.draw.line(screen, BLACK, (width_gap + i * cell_size, height_gap_top),
                          (width_gap + i * cell_size, height_gap_top + board_height), 1)
 
-    # make every third line thicker
+    # make every third line thicker (to represent the 3x3 squares)
     for i in range(1, 9):
         if i % 3 == 0:
             pygame.draw.line(screen, BLACK, (width_gap, height_gap_top + i * cell_size),
@@ -81,7 +81,7 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
             pygame.draw.line(screen, BLACK, (width_gap + i * cell_size, height_gap_top),
                              (width_gap + i * cell_size, height_gap_top + board_height), 2)
 
-    # Draw the pockets
+    # Draw the pockets for captured pieces
     pygame.draw.rect(screen, IVORY, (pocket_gap, height_gap_top, pocket_width, pocket_height), 0)
     pygame.draw.rect(screen, IVORY,
                      (pocket_gap, height_gap_top + board_height - pocket_height, pocket_width, pocket_height), 0)
@@ -91,8 +91,15 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
     pygame.draw.rect(screen, BLACK,
                      (pocket_gap, height_gap_top + board_height - pocket_height, pocket_width, pocket_height), 2)
 
-    # Font for text
+    # Default font for our text rendering
     font = pygame.font.SysFont(None, 30)
+
+    # Draw the color label for each pocket above the pocket
+    for i, color in enumerate(['White', 'Black']):
+        text = font.render(color, True, BLACK)
+        text_rect = text.get_rect(center=(pocket_gap + pocket_width // 2, height_gap_top - cell_size // 7))
+        screen.blit(text, text_rect if i == 0 else text.get_rect(center=(pocket_gap + pocket_width // 2,
+                                                                         height_gap_top + board_height + cell_size // 7)))
 
     # Add labels for columns above the board
     for i in [9, 8, 7, 6, 5, 4, 3, 2, 1]:
@@ -108,23 +115,31 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
             center=(width_gap - cell_size // 9, height_gap_top + (i - 1) * cell_size + cell_size // 2))
         screen.blit(text, text_rect)
 
-    dead_pieces = {'w': [], 'b': []}
     # Draw the pieces
+    # We'll track dead pieces separately to draw them in pockets later
+    dead_pieces = {'w': [], 'b': []}
+
+    # cycle through pieces
     for index, i in enumerate(piece_array):
+        # if the piece is selected, we'll draw it at the mouse position
         if selected_piece and index == selected_piece:
-            text = pygame.font.SysFont(None, 50).render(i.shorthand(), True, BLACK) if i.color == 'b' else pygame.font.SysFont(None, 50).render(i.shorthand(), True, GRAY)
+            text = pygame.font.SysFont(None, 50).render(i.shorthand(), True, BLACK) if i.color == 'b' \
+                else pygame.font.SysFont(None, 50).render(i.shorthand(), True, GRAY)
             text_rect = text.get_rect(center=(se_pos[0], se_pos[1]))
             screen.blit(text, text_rect)
+        # for all other pieces, we'll draw them at their current position
         elif i.is_alive:
             row, col = i.pos
-            text = pygame.font.SysFont(None, 50).render(i.shorthand(), True, BLACK) if i.color == 'b' else pygame.font.SysFont(None, 50).render(i.shorthand(), True, GRAY)
+            text = pygame.font.SysFont(None, 50).render(i.shorthand(), True, BLACK) if i.color == 'b' \
+                else pygame.font.SysFont(None, 50).render(i.shorthand(), True, GRAY)
             text_rect = text.get_rect(center=(width_gap + col * cell_size + cell_size // 2,
                                               height_gap_top + row * cell_size + cell_size // 2))
             screen.blit(text, text_rect)
+        # dead pieces are stored for next step
         elif not i.is_alive:
             dead_pieces[i.color].append(i.shorthand())
 
-    # Count occurrences of each piece type in the pocket
+    # Count occurrences of each piece type in the dead-list
     piece_counts = {'w': {}, 'b': {}}
     for j in dead_pieces:
         for i in dead_pieces[j]:
@@ -133,14 +148,16 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
             else:
                 piece_counts[j][i] = 1
 
-    # Draw the pieces
+    # Draw the dead pieces
     for j in dead_pieces:
         for index, i in enumerate(dead_pieces[j]):
+            # make sure we adjust our pixel offset to the correct pocket
             if j == 'w':
                 y_offset = height_gap_top
             else:
                 y_offset = height_gap_top + board_height - pocket_height
 
+            # map the piece role to a position in the pocket
             positions = {
                 'p': (pocket_gap + pocket_width // 4, y_offset + pocket_height // 4),
                 'l': (pocket_gap + pocket_width // 2, y_offset + pocket_height // 4),
@@ -151,17 +168,41 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
                 'r': (pocket_gap + 3 * pocket_width // 4, y_offset + 3 * pocket_height // 4)
             }
 
-            # if the pocket is white, we'll draw the pieces in gray
-
-            text = pygame.font.SysFont(None, 50).render(i, True, BLACK) if j == 'b' else pygame.font.SysFont(None, 50).render(i, True, GRAY)
+            # adjust the piece according to its color
+            text = pygame.font.SysFont(None, 50).render(i, True, BLACK) if j == 'b' \
+                else pygame.font.SysFont(None, 50).render(i, True, GRAY)
             text_rect = text.get_rect(center=positions[i])
             screen.blit(text, text_rect)
 
-            # Draw the count if there are multiple pieces of the same type
+            # Add a count subscript if there are multiple pieces of the same type in pocket
             if piece_counts[j][i] > 1:
                 count_text = pygame.font.SysFont(None, 20).render(str(piece_counts[j][i]), True, BLACK)
                 count_rect = count_text.get_rect(center=(positions[i][0], positions[i][1] + 20))
                 screen.blit(count_text, count_rect)
+
+    # Draw error text
+    if error_text:
+        # place the error text, in red, at the bottom of the screen below the board
+        text = pygame.font.SysFont(None, 20).render(str(error_text), True, (255, 0, 0))
+        text_rect = text.get_rect(center=(screen_width // 2, height_gap_top + board_height + 25))
+        screen.blit(text, text_rect)
+
+    # write text on the left of screen, between pockets, to indicate whose turn it is
+    player_color = 'Black' if active_color == 'b' else 'White'
+    text = pygame.font.SysFont(None, 40).render(f"{player_color}'s turn", True, BLACK)
+    text_rect = text.get_rect(center=(pocket_gap + pocket_width // 2, height_gap_top + board_height // 2))
+    screen.blit(text, text_rect)
+    # draw a light red arrow pointing to the current player
+    if player_color == 'White':
+        pygame.draw.polygon(screen, (255, 100, 100),
+                            [(pocket_gap + pocket_width // 2, height_gap_top + board_height // 2 - 50),
+                             (pocket_gap + pocket_width // 2 - 20, height_gap_top + board_height // 2 - 20),
+                             (pocket_gap + pocket_width // 2 + 20, height_gap_top + board_height // 2 - 20)])
+    elif player_color == 'Black':
+        pygame.draw.polygon(screen, (255, 100, 100),
+                            [(pocket_gap + pocket_width // 2, height_gap_top + board_height // 2 + 50),
+                             (pocket_gap + pocket_width // 2 - 20, height_gap_top + board_height // 2 + 20),
+                             (pocket_gap + pocket_width // 2 + 20, height_gap_top + board_height // 2 + 20)])
 
     # Update the display
     pygame.display.flip()
@@ -171,7 +212,7 @@ def draw_shogi_board_pygame(pa=None, save_path=None):
         pygame.image.save(screen, save_path)
 
 
-# initial safety variables
+# initial game-loop variables
 selected_piece = None  # index of the clicked piece
 piece_array = create_piece_array()  # array of pieces
 error_text = None  # possible error text that I might draw on screen if needed
@@ -204,10 +245,14 @@ def handle_input(input_event):
                 # if it's coming from pocket, we'll try to drop it rather than use the move command
                 if not piece_array[selected_piece].is_alive:
                     try:
-                        piece_array[selected_piece].place(position)
-                        selected_piece = None
-                        se_pos = None
-                        active_color = 'w' if active_color == 'b' else 'b'
+                        if not check_pos(piece_array, coord=position):
+                            piece_array[selected_piece].place(position)
+                            selected_piece = None
+                            se_pos = None
+                            active_color = 'w' if active_color == 'b' else 'b'
+                            error_text = None
+                        else:
+                            raise ValueError("illegal move: target location is occupied")
                     except ValueError as e:
                         error_text = e
                         selected_piece = None
@@ -218,6 +263,7 @@ def handle_input(input_event):
                         selected_piece = None
                         se_pos = None
                         active_color = 'w' if active_color == 'b' else 'b'
+                        error_text = None
                     except ValueError as e:
                         # if it doesn't work, we'll just reset the selected piece
                         error_text = e
@@ -228,6 +274,8 @@ def handle_input(input_event):
                     if piece_array[get_occupier(piece_array, coord=position)].color == active_color:
                         selected_piece = get_occupier(piece_array, coord=position)
                         se_pos = pygame.mouse.get_pos()
+                    else:
+                        error_text = "illegal move: not your piece"
 
         # next, we want to check if the click was in the top or bottom pocket
         elif pocket_gap <= x <= pocket_gap + pocket_width:
@@ -259,7 +307,6 @@ def handle_input(input_event):
                     piece = piece_map.get(pocket_position)
                     selected_piece = get_dead_piece(piece_array, pocket, piece)
                     se_pos = pygame.mouse.get_pos()
-
 
     if input_event.type == MOUSEMOTION:
         if selected_piece:
